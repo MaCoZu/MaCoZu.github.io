005129156749f1853f6debb745df44796ba05351
MaCoZu <marcoz@posteo.de>
2023-12-20 11:18:45 +0100

    all deleted

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/svelte-ts-starter/",
-  plugins: [svelte()],
-})
