70d5b2697cf12ded1d740542ff1c19bea33d2a9e
MaCoZu <marcoz@posteo.de>
2023-12-20 05:55:44 +0100

    some fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/svelte-starter/",
+  base: "/svelte-ts-starter/",
   plugins: [svelte()],
 })
