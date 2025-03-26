42283835778f563e188a790aa79d56f8072539d9
MaCoZu <marcoz@posteo.de>
2025-03-26 10:24:05 +0100

    mdx working with katex

--- astro2/astro.config.mjs
+++ astro2/astro.config.mjs
@@ -1,13 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import tailwindcss from "@tailwindcss/vite";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [tailwindcss()],   
         optimizeDeps: {
             include: ['d3']
         },
     },
+    integrations: [
+        mdx({
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex],
+        }),
+    ],
 });
+
+
+
